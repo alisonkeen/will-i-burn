da4633d9fbb31382355f79a5d31ca3bf2b1455bb
Alex <alexg-hacks@users.noreply.github.com>
2016-07-30 14:55:29 +0930

    Get region ID

--- PSQL/get_region_from_lat_long.sql
+++ PSQL/get_region_from_lat_long.sql
@@ -1,1 +1,1 @@
-select region from sagovtregions where st_within(ST_GeomFromEWKT('SRID=4326; POINT(138.5887884 -34.9220317 )'),geom);
+select gid, region from sagovtregions where st_within(ST_GeomFromEWKT('SRID=4326; POINT(138.5887884 -34.9220317 )'),geom);
