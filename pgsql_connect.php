06eef4b8275abec8db5d5261507fb8c24ca9fe72
Daniel Sobey <dns@dns.id.au>
2016-07-30 16:34:34 +0930

    This now populates the option list from the database

--- pgsql_connect.php
+++ pgsql_connect.php
@@ -2,10 +2,9 @@
  $dbconn = pg_connect("dbname=govhack port=5432 host=localhost user=govhack password=govhack") or die("Could not connect");
 
   $stat = pg_connection_status($dbconn);
-  if ($stat === PGSQL_CONNECTION_OK) {
-      echo 'Connection status ok';
-      pg_close($dbconn);
-  } else {
-      echo 'Connection status bad';
+  if ($stat != PGSQL_CONNECTION_OK) {
+      echo 'Error connecting to database';
+      echo 'please contact some to take a look at this :(';
+     exit;
   }    
 ?>
