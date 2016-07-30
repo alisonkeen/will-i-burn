6cf0eca13a85edc580370822fa87978ccd43a71e
Alison Keen <ali.keen@gmail.com>
2016-07-30 16:50:36 +0930

    Updated navbar and added an 'about' page for fun

---
+++ about.php
@@ -0,0 +1,98 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Will I Burn? Govhack 2016">
+    <meta name="author" content="">
+    <!-- <link rel="icon" href="../../favicon.ico"> -->
+
+    <title>Will I Burn? | Govhack Unleashed 2016</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug 
+         File Not included in version of bootstrap downloaded?
+    <link href="bootstrap-3.3.7-dist/css/ie10-viewport-bug-workaround.css" rel="stylesheet"> -->
+
+    <!-- Custom styles for this template -->
+    <link href="bootstrap-3.3.7-dist/css/starter-template.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="bootstrap-3.3.7-dist/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <?php include 'navbar.php' ?>
+
+
+	<div class="container">
+	<h1> About Team "Red Hot" </h1>
+
+	<div class="row">
+
+	<form action="results2.php">
+
+	   <fieldset>
+
+           <div class="col-md-12">
+               <p> We are a bunch of programmers from Adelaide, who are otherwise unaffiliated. </p> 
+           </div>
+
+           <div class="col-md-4">
+               <h2> Alex </h2> 
+              <p> Alex is a programming genius who has contributed by working on the R API and PostgreSQL operations to help make sense of the data </p>
+           </div>
+
+           <div class="col-md-4">
+               <h2> Tim </h2> 
+              <p> Tim has been tirelessly churning through large amounts of bushfire, temperature and climate prediction data using R in search of trends, interesting information and patterns </p>
+           </div>
+
+           <div class="col-md-4">
+               <h2> Rowan </h2> 
+              <p> Rowan has been churning through GIS data to help make sense of the places and spaces involved in past and potential bushfires </p> 
+           </div>
+
+           <div class="col-md-4">
+               <h2> Daniel </h2> 
+              <p> Daniel has served as webmaster, systems administrator, resident Linux expert, genius at all programming languages (especially SQL/PostgreSQL) and manager of domains </p>
+           </div>
+
+           <div class="col-md-4">
+               <h2> Alison Keen </h2> 
+              <p> Alison has been doing bits and pieces where she can, such as creating forms and basic interfaces, updating the team hackerspace, and generally nodding along and smiling. </p>
+           </div>
+
+
+   </fieldset>
+</form>
+
+</div> <!-- End Row -->
+</div> <!-- End Container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="bootstrap-3.3.7-dist/js/jquery.min.js"><\/script>')</script>
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="bootstrap-3.3.7-dist/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
+
+
--- navbar.php
+++ navbar.php
@@ -12,9 +12,8 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="index.php">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li class="active"><a href="index.php">Back to Start</a></li>
+            <li><a href="about.php">About</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
