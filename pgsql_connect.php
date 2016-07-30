80fc4f56de27117538c76e1d46dd7005aa4f7d2d
Alison Keen <ali.keen@gmail.com>
2016-07-30 15:28:21 +0930

    Fixed connection string, deleted phpinfo() function from server

--- pgsql_connect.php
+++ pgsql_connect.php
@@ -1,5 +1,6 @@
 <?php
-  $dbconn = pg_connect("dbname=govhack user=govhack password=govhack") or die("Could not connect");
+ $dbconn = pg_connect("dbname=govhack port=5432 host=localhost user=govhack password=govhack") or die("Could not connect");
+
   $stat = pg_connection_status($dbconn);
   if ($stat === PGSQL_CONNECTION_OK) {
       echo 'Connection status ok';
