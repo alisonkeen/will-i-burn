86404e44548e2dcdfb97fd1b18f5ef8a92cc3eb4
Alex <alexg-hacks@users.noreply.github.com>
2016-07-30 19:27:26 +0930

    Tim's bug fixes

--- R/web-api-endpoints.R
+++ R/web-api-endpoints.R
@@ -1,22 +1,19 @@
 library(caret)
 library(rpart)
 
-df = read.csv("FAKE_Unleashed_Data_Framework.csv")
+df <- read.csv("FAKE_Unleashed_Data_Framework.csv")
 
-model.perc <- train(HectarePerc ~ Month+Region+Dry+Temp+Above.35+Burnoff, data = df, method = "rpart")
+model.perc <- train(HectarePerc ~ Month+Region+Temp+Above.35, data = df, method = "lm")
 
 #* @get /perc
-perc <- function(month, region, dry, temp, above.35, burnoff){
-
+perc <- function(month, region, temp, above.35){
+  
   Month <- as.numeric(month)
   Region <- as.numeric(region)
-  Dry <- as.numeric(dry)
   Temp <- as.numeric(temp)
   Above.35 <- as.numeric(above.35)
-  Burnoff <- as.numeric(burnoff)
 
-  newdata <- data.frame(Month, Region, Dry, Temp, Above.35, Burnoff)
-  colnames(newdata) <- c("Month", "Region", "Dry", "Temp", "Above.35", "Burnoff")
+  newdata <- data.frame(Month, Region, Temp, Above.35)
   predict(model.perc, newdata = newdata)
 }
 
