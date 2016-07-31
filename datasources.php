92f804e615a6b41fc979f95c5c5edf0a3177b946
Daniel Sobey <dns@dns.id.au>
2016-07-31 17:05:28 +0930

    Added page with data sources used

---
+++ datasources.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Will I Burn? Govhack 2016">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Will I Burn? | Govhack Unleashed 2016</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug
+         File Not included in version of bootstrap downloaded?
+    <link href="bootstrap-3.3.7-dist/css/ie10-viewport-bug-workaround.css" rel="stylesheet"> -->
+
+    <!-- Custom styles for this template -->
+    <link href="bootstrap-3.3.7-dist/css/starter-template.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="bootstrap-3.3.7-dist/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <?php include 'navbar.php' ?>
+
+
+        <div class="container">
+        <h1> Data used in Model Development </h1>
+
+        <div class="row">
+<h3>Bushfires and Prescribed Burns History</h3>
+<p>This dataset provides state-wide fire scar mapping for major bushfires that have occurred within South Australia. It also provides fire scar mapping for prescribed burning activities that have occurred on land managed by the State Government Agencies (Department of Environment, Water and Natural Resources, Forestry SA and SA Water). A landscape approach is used for fire history mapping but may be incomplete for a given reserve and region. “Burnoffs” on private land are excluded from this dataset.</p>
+<a href="https://data.sa.gov.au/data/dataset/fire-history">https://data.sa.gov.au/data/dataset/fire-history</a>
+
+</div>
+<div class="row">
+<h3>SA Government Regions</h3>
+<p>South Australian Government Regions facilitate the planning, monitoring and service delivery of government functions. The regions align to whole local government boundaries, with only one exception out of 68 councils - the separation of Enfield from the Port Adelaide Enfield Council.</p>
+<a href="https://data.sa.gov.au/data/dataset/south-australian-government-regions">https://data.sa.gov.au/data/dataset/south-australian-government-regions</a>
+
+</div>
+<div class="row">
+<h3>Roads</h3>
+</p>Statewide Road Network including sealed and unsealed roads. The dataset represents navigable roads, including public and private access roads and tracks. A separate data layer stores 'unformed' DCDB centrelines which do not represent navigable roads. A limited number of associated features are stored separately as point features. Automatically updated when changes occour</p>
+<a href="http://data.sa.gov.au/data/dataset/roads">http://data.sa.gov.au/data/dataset/roads</a>
+</div>
+<div class="row">
+<h3>Population by Age and Sex</h3>
+Estimates of the resident population by age (in five-year age groups up to 85 and over) and sex.
+<a href="https://data.sa.gov.au/data/dataset/population-by-age-and-sex">https://data.sa.gov.au/data/dataset/population-by-age-and-sex</a>
+</div>
+        <div class="row">
+<h3>Temperature data</h3>
+Data downloaded from the Bureau of Meteorology
+<a href="http://www.bom.gov.au/climate/data/">http://www.bom.gov.au/climate/data/</a>
+Stations:
+<ul>
+<li>Kent Town – Adelaide Metro</li>
+<li>Lake Victoria – Murray and Mallee</li>
+<li>Mount Gambier Aero – South East</li>
+<li>Rosedale – Barossa</li>
+<li>Cape Willoughby – Flerieu</li>
+<li>Yongola – Yorke and Mid north</li>
+<li>Ceduna – Eyre and Western</li>
+<li>Woomera – Far North</li>
+</ul>
+
+       </div> <!-- End Row -->
+    </div> <!-- End Container -->
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="bootstrap-3.3.7-dist/js/jquery.min.js"><\/script>')</script>
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="bootstrap-3.3.7-dist/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
+
--- navbar.php
+++ navbar.php
@@ -19,11 +19,23 @@ echo "            <li class=\"active\"><a href=\"index.php\">Back to Start</a></
 else {
 echo "            <li><a href=\"index.php\">Back to Start</a></li>";
 }
+if ( $_SERVER['PHP_SELF'] =='/results.php'){
+echo "            <li class=\"active\"><a href=\"index.php\">Results</a></li>";
+}else{
+echo "            <li><a href=\"index.php\">Results</a></li>";
+}
 if ( $_SERVER['PHP_SELF'] =='/dev.php'){
 echo "            <li class=\"active\"><a href=\"dev.php\">Development process</a></li>";
 }else{
 echo "            <li><a href=\"dev.php\">Development process</a></li>";
 }
+
+if ( $_SERVER['PHP_SELF'] =='/datasources.php'){
+echo "            <li class=\"active\"><a href=\"datasources.php.php\">Data sources used</a></li>";
+}else{
+echo "            <li><a href=\"datasources.php\">Data sources used</a></li>";
+}
+
 if ( $_SERVER['PHP_SELF'] =='/findings.php'){
 echo "            <li class=\"active\"><a href=\"findings.php\">Findings</a></li>";
 }else{
