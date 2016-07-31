7b7fcaf27dad10277c39833f112d310628d2f51d
Alex <alexg-hacks@users.noreply.github.com>
2016-07-31 11:36:30 +0930

    Use CURL to call API

--- api_fns.php
+++ api_fns.php
@@ -1,15 +1,28 @@
 <?php
 
-function chance_of_fire(int $region, int $month, int $temp, int $hot_days, int $backburning)
+function http_get($url)
 {
-   $odds_of_fire = 50;
-   // this is where the query goes... 
+   $cinit = curl_init();
+   curl_setopt($cinit, CURLOPT_URL, $url);
+   curl_setopt($cinit, CURLOPT_HEADER, 0);
+   curl_setopt($cinit, CURLOPT_TIMEOUT, 10);
+   curl_setopt($cinit, CURLOPT_FOLLOWLOCATION, 1);
+   curl_setopt($cinit, CURLOPT_RETURNTRANSFER, 1);
+   $response = curl_exec($cinit);
+   curl_close($cinit);
+   return $response;
+}
 
+
+function chance_of_fire($region, $month, $temp, $hot_days, $backburning)
+{
    $query_url = "http://localhost:8123/perc?month=$month&region=$region&dry=0&temp=$temp&above.35=$hot_days&burnoff=$backburning";
 
-   // TODO run *actual* query as above. 
-
-   return $odds_of_fire;
+   $response = http_get($query_url);
+   $probability_of_fire = trim($response, "[] ");
+   $percentage = (int)($probability_of_fire * 100);
+   
+   return $percentage;
 }
 
 ?>
