f83e49378156efc8164a97befb76297bc8a6e95a
Alex <alexg-hacks@users.noreply.github.com>
2016-07-31 10:36:04 +0930

    Use global DB connection

--- query_fns.php
+++ query_fns.php
@@ -1,18 +1,18 @@
 <?php
 
-
-
 function get_region_name(int $region_num)
 {
+   global $dbconn;
+
    $regions = pg_query($dbconn, "select region from sagovtregions where gid = $region_num");
    if (!$regions) {
      return "Not found ($region_num)";
    }
 
-   return $regions[0];
+   return pg_fetch_result($regions[0], 0);
 }
 
-function chance_of_fire(int $region, int $month, int $temp, int $hot_days, int $backburning )
+function chance_of_fire(int $region, int $month, int $temp, int $hot_days, int $backburning)
 {
    $odds_of_fire = 50;
    // this is where the query goes... 
@@ -23,4 +23,5 @@ function chance_of_fire(int $region, int $month, int $temp, int $hot_days, int $
 
    return $odds_of_fire;
 }
+
 ?>
