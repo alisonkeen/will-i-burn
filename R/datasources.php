fdceedf3c662c7629a0d85ec2b6b63bcb4ca42c0
timhouse2016 <timothyjohnhouse@gmail.com>
2016-07-31 16:13:09 +0930

    Datasources.php

---
+++ R/datasources.php
@@ -0,0 +1,13 @@
+Data used in Model Development
+DEWNR Bushfire data  - https://data.sa.gov.au/data/dataset/fire-history
+Temperature data 
+http://www.bom.gov.au/climate/data/ 
+Stations
+•	Kent Town – Adelaide Metro
+•	Lake Victoria – Murray and Mallee
+•	Mount Gambier Aero – South East
+•	Rosedale – Barossa
+•	Cape Willoughby – Flerieu
+•	Yongola – Yorke and Mid north
+•	Ceduna – Eyre and Western
+•	Woomera – Far North
